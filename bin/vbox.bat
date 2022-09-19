f36d6f6d9e609c13520fb3f54e0b177e84857598
K. Matthaus EHON <ntdash@outlook.com>
2022-09-19 03:01:12 +0000

    feat: `vbox` command

---
+++ bin/vbox.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+set cmd_name="vbox"
+set exe_pathname="%scripts_path%\utils\.bootstrap"
+
+if not exist %exe_pathname% ( goto :not_found )
+
+set argv="%cmd_name%" %*
+
+bash -l %exe_pathname% %argv%
+
+goto :end
+
+:not_found
+echo %cmd_name% is not recognized as an internal or external command, operable program or batch file. && goto :end
+
+:bootstrap_error
+echo "bash plugin main file `.bootstrap` is missing." && goto :end
+
+:end
+endlocal
---
+++ commands/vbox/utils/.profile
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+export form_profile="I'm the current profile"
